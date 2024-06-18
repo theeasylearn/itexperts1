45776eabf877d2d119a7af86806f25592a7c23e6
theeasylearn <theeasylearn@gmail.com>
2024-06-18 17:00:13 +0530

    add course

--- php/project/add_course.php
+++ php/project/add_course.php
@@ -1,5 +1,5 @@
-<?php 
-    require_once('inc/header-part.php');
+<?php
+require_once('inc/header-part.php');
 ?>
 </head>
 
@@ -13,35 +13,47 @@
     </div>
     <div class="container white-form">
         <form action="">
-            <table>
+            <table id="input-table">
                 <tr>
-                    <td></td>
-                    <td></td>
+                    <td width='33%'>Title</td>
+                    <td>
+                        <input type="text" name="title" id="title" class="input-box" required />
+                    </td>
                 </tr>
                 <tr>
-                    <td></td>
-                    <td></td>
+                    <td>Fees</td>
+                    <td>
+                        <input type="number" name="fees" id="fees" class="input-box" required />
+                    </td>
                 </tr>
                 <tr>
-                    <td></td>
-                    <td></td>
+                    <td>Duration (in days)</td>
+                    <td>
+                        <input type="number" name="duration" id="duration" class="input-box" required />
+                    </td>
                 </tr>
                 <tr>
-                    <td></td>
-                    <td></td>
+                    <td>description</td>
+                    <td>
+                        <textarea class="input-box" name="description" id="description">
+
+                        </textarea>
+                    </td>
                 </tr>
                 <tr>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
+                    <td colspan="2" align="center">
+                        <button type="submit" class="save">
+                            <i class="fa fa-save"></i> Save
+                        </button>
+                        <button type="reset" class="clear">
+                            <i class="fa fa-trash"></i> clear all
+                        </button>
+                    </td>
                 </tr>
             </table>
         </form>
     </div>
 </body>
-<?php 
-    require_once('inc/footer.php');
+<?php
+require_once('inc/footer.php');
 ?>
